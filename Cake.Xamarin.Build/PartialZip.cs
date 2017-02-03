49677714783bacd66ee637b626945de4910a50a0
Redth <jondick@gmail.com>
2017-02-03 13:55:04 -0500

    Fix ZipEntryInfo

--- Cake.Xamarin.Build/PartialZip.cs
+++ Cake.Xamarin.Build/PartialZip.cs
@@ -1,10 +1,101 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Cake.Core.IO;
+using ICSharpCode.SharpZipLib.Zip;
+
 namespace Cake.Xamarin.Build
 {
-	public class PartialZip
+	public class ZipEntryInfo
 	{
-		public PartialZip()
+		public long RangeStart { get; set; }
+		public string EntryName { get; set; }
+		public long CompressedSize { get; set; }
+
+		public long RangeEnd
 		{
+			get { return RangeStart + CompressedSize; }
+		}
+	}
+
+	internal static class PartialZip
+	{
+		internal static Stream ReadZipEntry(string zipFilename, string entryName)
+		{
+			using (var fs = File.OpenRead(zipFilename))
+			using (var zipFile = new ZipFile(fs))
+			{
+				var entry = zipFile.GetEntry(entryName);
+
+				if (entry != null)
+					return zipFile.GetInputStream(entry);
+
+
+			}
+
+			return null;
+		}
+
+		internal static string ReadEntryText(string zipFilename, string entryName, bool readBinaryAsHex = false)
+		{
+			using (var entryStream = ReadZipEntry(zipFilename, entryName))
+			{
+				if (entryStream == null)
+					return null;
+
+				if (readBinaryAsHex)
+				{
+					using (var ms = new MemoryStream())
+					{
+						entryStream.CopyTo(ms);
+
+						var data = ms.ToArray();
+						var sb = new System.Text.StringBuilder();
+						for (int i = 0; i < data.Length; i++)
+							sb.Append(data[i].ToString("X2"));
+
+						return sb.ToString().ToLower();
+					}
+				}
+				else
+				{
+					string result = null;
+
+					using (var sr = new StreamReader(entryStream))
+						result = sr.ReadToEnd();
+
+					return result;
+				}
+			}
+		}
+
+		internal static List<ZipEntryInfo> FindZipFileRanges(string zipFilename)
+		{
+			var downloadInfo = new List<ZipEntryInfo>();
+
+			using (var fs = File.OpenRead(zipFilename))
+			using (var zipFile = new ZipFile(fs))
+			{
+				var entriesEnumerator = zipFile.GetEnumerator();
+
+				while (entriesEnumerator.MoveNext())
+				{
+					var entry = entriesEnumerator.Current as ZipEntry;
+
+					// DRAGONS!  Using a private method to get the location of the start of the entry in the zip file
+					var locateEntryMethod = zipFile.GetType().GetMethod("LocateEntry", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+					var locateOffset = (long)locateEntryMethod.Invoke(zipFile, new object[] { entry });
+
+					downloadInfo.Add(new ZipEntryInfo
+					{
+						EntryName = entry.Name,
+						RangeStart = locateOffset,
+						CompressedSize = entry.CompressedSize,
+					});
+				}
+
+				return downloadInfo;
+			}
 		}
 	}
 }
