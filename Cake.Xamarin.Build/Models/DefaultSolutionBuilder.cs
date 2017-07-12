883b28dac253fac604f7ac9297fc5b9c44afe802
Redth <jondick@gmail.com>
2017-07-11 21:03:43 -0400

    Switch to MSBuild by default

--- Cake.Xamarin.Build/Models/DefaultSolutionBuilder.cs
+++ Cake.Xamarin.Build/Models/DefaultSolutionBuilder.cs
@@ -23,6 +23,7 @@ namespace Cake.Xamarin.Build
             Platform = "\"Any CPU\"";
             Configuration = "Release";
             Properties = new Dictionary<string, List<string>> ();
+			AlwaysUseMSBuild = true;
         }
 
         public int? MaxCpuCount { get; set; }
@@ -43,7 +44,7 @@ namespace Cake.Xamarin.Build
         public OutputFileCopy [] OutputFiles { get; set; }
         public virtual string OutputDirectory { get; set; }
         public virtual bool RestoreComponents { get; set; }
-		public virtual bool AlwaysUseMSBuild { get; set; } = false;
+		public virtual bool AlwaysUseMSBuild { get; set; }
 		public virtual Core.Diagnostics.Verbosity? Verbosity { get; set; }
 
         public Action PreBuildAction { get;set; }
